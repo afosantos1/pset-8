c792e93b24a87ee77d068477db59ed1b576a8d55
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2020-02-06 10:00:39 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,3 @@
-///// CONSTANTS /////
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,7 +9,6 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-///// APP STATE (VARIABLES) /////
 let board;
 let turn;
 let win;
@@ -18,22 +16,16 @@ let started;
 let xScore = 0;
 let yScore = 0;
 
-///// CACHED ELEMENT REFERENCES /////
 const squares = Array.from(document.querySelectorAll("#board div"));
 const turnChoice = document.getElementById("turnChoice");
 const turnUpdate = document.getElementById("turnUpdate");
 const scores = document.getElementById("scorekeeper");
 const turnButtons = document.getElementById("turnButtons");
 
-///// EVENT LISTENERS /////
 window.onload = init;
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
 
-/**
- * Runs to check which turn choice button was clicked, and acts accordingly with variables turn
- * and started.
- */
 function addEventListeners() {
     turnButtons.addEventListener("click", function(event) {
         let clicked = event.target;
@@ -51,11 +43,6 @@ function addEventListeners() {
     });
 }
 
-///// FUNCTIONS /////
-
-/**
- * Function that runs on page startup to assign variable values and to render the board.
- */
 function init() {
     board = [
         "", "", "",
@@ -70,9 +57,6 @@ function init() {
     render();
 }
 
-/**
- * Responsible for updating the board appearance by looping through the board array.
- */
 function render() {
     board.forEach(function(mark, index) {
         squares[index].textContent = mark;
@@ -84,11 +68,7 @@ function render() {
     turnUpdate.textContent = (win === "T" ? `TIE GAME` : (win ? `${win} WINS` : `TURN: ${turn}`));
 }
 
-/**
- * Alternates between placing an X or an O on the physical board, depending on the turn state of the game.
- * @param event The targeted HTML element
- */
-function takeTurn(event) {
+n takeTurn(event) {
     if (started) {
         let index;
         if (!win) {
@@ -107,12 +87,6 @@ function takeTurn(event) {
     }
 }
 
-/**
- * Loops through the board array and checks against each of the programmed win conditions.
- * Checks whether winner is defined, and then whether the board has been completely filled. Winner is then assigned
- * a value accordingly.
- * @return Either "X", "O", "T", or null, depending on the value of winner
- */
 function getWinner() {
     let winner = null;
 
