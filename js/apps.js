7060cfafd6c6c7671bd726eb58966cb920d8e09d
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2020-02-08 00:03:57 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,5 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
+
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -11,48 +12,34 @@ const winningConditions = [
 ];
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
+var clearapp = document.getElementById("whosturn");
 let board;
-let turn;
+let turn = "X";
 let win;
 let scoreX = 0;
 let scoreO = 0;
-
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
 const squares = Array.from(document.querySelectorAll("#board div"));
 const message = document.querySelector("h2");
-const scoreXMessage = document.querySelector("h3");
-const scoreOMessage = document.querySelector("h4");
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
+document.getElementById("xfirst_bttn").onclick = xFirst;
+document.getElementById("ofirst_bttn").onclick = oFirst;
 window.onload = init;
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
 function init() {
-  board = [
-    "", "", "",
-    "", "", "",
-    "", "", ""
-  ];
-
-  do {
-    var initialPlayer = prompt("Enter X or O to declare the first player: ");
-    if (initialPlayer === null) {
-      turn = "X";
-      break;
-    } else if (initialPlayer === "X" || initialPlayer === "x") {
-      turn = "X";
-    } else if (initialPlayer === "O" || initialPlayer === "o") {
-      turn = "O";
-    } else {
-      //intentially blank
-    }
-  } while (initialPlayer !== "X" && initialPlayer !== "x" && initialPlayer !== "O" && initialPlayer !== "o");
-
+  board = ["", "", "", "", "", "", "", "", ""];
+  turn = "X";
   win = null;
 
   render();
+  document.getElementById("xfirst_bttn").style.visibility = "visible";
+  document.getElementById("ofirst_bttn").style.visibility = "visible";
 }
 
 function render() {
@@ -61,18 +48,7 @@ function render() {
   });
 
   message.textContent =
-    win === "T" ? "It's a tie!"
-    : win ? `${win} wins!`
-    : `Turn: ${turn}`;
-  if (win === "X") {
-    scoreX++;
-    scoreXMessage.textContent = scoreX;
-  } else if (win === "O") {
-    scoreO++;
-    scoreOMessage.textContent = scoreO;
-  } else {
-    //if there is a tie, do not change score
-  }
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
 }
 
 function takeTurn(e) {
@@ -98,11 +74,36 @@ function getWinner() {
     if (
       board[condition[0]] &&
       board[condition[0]] === board[condition[1]] &&
-      board[condition[0]] === board[condition[2]]
-    ){
-        winner = board[condition[0]];
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+//Feature Request #1
+    if (winner === "X") {
+      scoreX++;
+      document.getElementById('x-score').innerHTML = scoreX;
+    } else {
+      scoreO++;
+      document.getElementById('o-score').innerHTML = scoreO;
+    }
     }
   });
 
   return winner ? winner : board.includes("") ? null : "T";
+}
+
+//Feature Request #2
+function xFirst() {
+  init();
+  document.getElementById("whosturn").innerHTML = "Turn: X";
+  turn = "X";
+  document.getElementById("xfirst_bttn").style.visibility = "hidden";
+  document.getElementById("ofirst_bttn").style.visibility = "hidden";
+}
+
+function oFirst() {
+  init();
+  document.getElementById("whosturn").innerHTML = "Turn: O";
+  turn = "O";
+  document.getElementById("ofirst_bttn").style.visibility = "hidden";
+  document.getElementById("xfirst_bttn").style.visibility = "hidden";
 }
